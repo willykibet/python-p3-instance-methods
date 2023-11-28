3446523dc2875b9d332e91ac9c6d13fe3f22d6b7
willykibet <willykipchumba22@gmail.com>
2023-11-28 22:23:45 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,9 @@
 
 class Dog:
     # Class body goes here
-
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
     #Instance method definition
     pass
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,9 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
     #Instance method definition
+    def walk(self):
+        print( "The person is walking.")
     pass
